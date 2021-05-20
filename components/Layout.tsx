b268572500813fcf463032047a1e53e6af931b84
Joao <joao@joao.codes>
2021-05-20 23:22:53 +0100

    style: small style changes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,9 @@
 type Children = { children: React.ReactNode }
 
 const Layout = ({ children }: Children): JSX.Element => (
-  <div className="flex justify-center items-center h-screen">{children}</div>
+  <div className="flex justify-center items-center h-screen bg-white dark:bg-[#24292e]">
+    {children}
+  </div>
 )
 
 export default Layout
--- components/PolyworkWarn.tsx
+++ components/PolyworkWarn.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 
 const PolyworkWarn = (): JSX.Element => (
   <a href="https://www.poly.joao.codes/" className="flex justify-center fixed">
-    <div className="py-3 px-5 bg-white bg-opacity-80 overflow-hidden flex w-full md:w-[60%] lg:w-[40%] xl:w-[38%] justify-evenly items-center md:rounded-bl-md md:rounded-br-md select-none text-justify fixed top-0 shadow-md">
+    <div className="py-3 px-5 bg-white bg-opacity-[85] overflow-hidden flex w-full md:w-[60%] lg:w-[40%] xl:w-[38%] justify-evenly items-center md:rounded-bl-md md:rounded-br-md select-none text-justify fixed top-0 shadow-md">
       <div className="pr-3 flex flex-wrap">
         <Image src="/polywork.svg" width={52} height={52} />
       </div>
-      <p>
+      <p className="text-[#24292e]">
         Hey there! If you were looking for my <b>Polywork</b> profile, click
         here!
       </p>
