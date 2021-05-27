455ac2f733b69e572f5c1b76c0e589af018a5a38
Joao <joao@joao.codes>
2021-05-27 22:23:34 +0100

    feat: add watercolored background

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,13 @@
+import WaterColor from '@components/WaterColor'
 type Children = { children: React.ReactNode }
 
-const Layout = ({ children }: Children): JSX.Element => (
-  <div className="flex flex-col justify-center items-center min-h-screen py-8 lg:py-24 lg:pb-8 px-8 lg:px-0 bg-white dark:bg-codes-gray-1">
-    {children}
-  </div>
-)
+const Layout = ({ children }: Children): JSX.Element => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen py-8 lg:py-24 lg:pb-8 px-8 lg:px-0 bg-white dark:bg-codes-gray-1">
+      <WaterColor />
+      {children}
+    </div>
+  )
+}
 
 export default Layout
---
+++ components/WaterColor.tsx
@@ -0,0 +1,78 @@
+import Particles from 'react-tsparticles'
+
+// greatly inspired by Amelia Wattenberger's page @ https://wattenberger.com/
+const WaterColor = (): JSX.Element => {
+  return (
+    <Particles
+      id="tsparticles"
+      canvasClassName="dark:filter dark:blur-[100px] dark:brightness-[0.5] filter blur-[100px]"
+      options={{
+        fpsLimit: 60,
+        emitters: {
+          direction: 'none',
+          rate: {
+            delay: 10,
+            quantity: 5,
+          },
+        },
+        particles: {
+          number: {
+            value: 7,
+          },
+          color: {
+            value: [
+              '#4D816A',
+              '#7BAB9B',
+              '#398974',
+              '#035546',
+              '#D1D5C6',
+              '#F0ECAC',
+              '#ffffff',
+            ],
+          },
+          shape: {
+            type: 'circle',
+          },
+          opacity: {
+            value: 0.7,
+          },
+          size: {
+            value: 180,
+            anim: {
+              enable: true,
+              speed: 200,
+              count: 1,
+              size_min: 0,
+              sync: true,
+              startValue: 'min',
+              destroy: 'none',
+            },
+          },
+          move: {
+            enable: true,
+            speed: 0.4,
+            direction: 'none',
+            random: true,
+            trail: {
+              enable: false,
+              length: 5,
+              fillColor: '#fff',
+            },
+            outModes: {
+              default: 'destroy',
+            },
+          },
+        },
+        interactivity: {
+          detectsOn: 'canvas',
+          events: {
+            resize: true,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  )
+}
+
+export default WaterColor
