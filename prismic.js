ddb7ae90bda400a926757ea3fcc5dca1bbb6a5ad
João <joao@joao.codes>
2021-07-19 19:47:43 +0100

    feat: drop prismic query for a query to a cf worker (#14)
    * remove all @prismicio/client deps
    
    * fetch worker url

--- prismic.js
+++
@@ -1,7 +0,0 @@
-import Prismic from '@prismicio/client'
-
-const apiEndpoint = process.env.PRISMIC_ENDPOINT
-const accessToken = process.env.PRISMIC_TOKEN
-
-export const Client = (req = null, options = {}) =>
-  Prismic.client(apiEndpoint, { accessToken })
