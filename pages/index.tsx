a1a6c80a6753b70e3610f6a5f255fff035a41826
João <joao@joao.codes>
2021-08-17 19:33:51 +0100

    remove polywork notice on top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import * as prismic from '@prismicio/client'
-import PolyworkWarn from '@components/PolyworkWarn'
 import Layout from '@components/Layout'
 import ModeDropdown from '@components/ModeDropdown'
 import IntroText from '@components/IntroText'
@@ -47,7 +46,6 @@ const IndexPage = ({
             rel="stylesheet"
           />
         </Head>
-        <PolyworkWarn />
         <ModeDropdown />
         {api_error ? (
           <h1 className="leading-relaxed text-5xl text-red-500">
