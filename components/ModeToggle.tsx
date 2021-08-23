812e2e5205120c43705432db63ff5223202f1ecb
João <joao@joao.codes>
2021-08-23 17:14:10 +0000

    cleanup on last commit

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -7,9 +7,6 @@ import MoonIcon from '@components/icons/MoonIcon'
 const ModeToggle = (): JSX.Element => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
-  useEffect(() => {
-    console.log(theme)
-  }, [theme])
 
   // show component only after page is mounted so that theme isn't undefined (hydration mismatch)
   // https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
@@ -49,38 +46,6 @@ const ModeToggle = (): JSX.Element => {
         <MoonIcon />
       </button>
     </div>
-    /** <Menu as="div" className="fixed top-7 md:right-16 right-5">
-      <Menu.Button
-        aria-label="change display mode"
-        className="flex flex-row justify-center items-center rounded-md shadow-lg px-4 py-2 border-0 hover:mix-blend-difference focus:outline-none bg-white dark:bg-codes-gray-3 filter hover:brightness-90"
-      >
-        <span>{getCurrentThemeIconComponent(theme)}</span>
-        <span>
-          <DropdownIcon />
-        </span>
-      </Menu.Button>
-      <Transition
-        enter="transition duration-300 ease-out"
-        enterFrom="transform scale-90 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-300 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-90 opacity-0"
-      >
-        <Menu.Items className="flex-col items-center flex w-full bg-white filter brightness-90 dark:bg-codes-gray-3 mt-4 rounded-md">
-          {modes.map(mode => (
-            <Menu.Item key={mode} disabled={mode === theme}>
-              <button
-                className={`filter hover:brightness-90 bg-white dark:bg-codes-gray-3 py-2 px-4 rounded-md w-full flex justify-center`}
-                onClick={() => setTheme(mode)}
-              >
-                <span>{getCurrentThemeIconComponent(mode)}</span>
-              </button>
-            </Menu.Item>
-          ))}
-        </Menu.Items>
-      </Transition>
-    </Menu>*/
   )
 }
 
