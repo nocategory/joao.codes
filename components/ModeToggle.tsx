2a236f033480452ee344faa2a7350244e0f55421
João <joao@joao.codes>
2021-08-23 17:11:30 +0000

    add ability to swap theme on icon click

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -19,9 +19,12 @@ const ModeToggle = (): JSX.Element => {
 
   return (
     <div className="fixed top-7 md:right-16 right-5 flex md:scale-110 lg:scale-[1.20]">
-      <span className={`${theme !== 'light' ? 'opacity-40' : ''}`}>
+      <button
+        className={`${theme !== 'light' ? 'opacity-40' : ''}`}
+        onClick={() => (theme !== 'light' ? setTheme('light') : null)}
+      >
         <SunIcon />
-      </span>
+      </button>
       <Switch
         checked={theme === 'light' ? false : true}
         onChange={
@@ -30,7 +33,7 @@ const ModeToggle = (): JSX.Element => {
         defaultChecked={false}
         className={`${
           theme === 'dark' ? 'bg-white/20' : 'bg-black/40'
-        } relative inline-flex items-center h-6 w-11 mx-2 rounded-full transition-colors backdrop-brightness-75 dark:backdrop-brightness-150`}
+        } relative inline-flex items-center h-6 w-11 mx-2 rounded-full transition-all backdrop-brightness-75  hover:backdrop-brightness-50 dark:backdrop-brightness-150 dark:hover:backdrop-brightness-50`}
       >
         <span className="sr-only">Switch between light and dark theme</span>
         <span
@@ -39,9 +42,12 @@ const ModeToggle = (): JSX.Element => {
           } inline-block w-4 h-4 transform bg-white/100 rounded-full transition-transform duration-300`}
         />
       </Switch>
-      <span className={`${theme !== 'dark' ? 'opacity-40' : ''}`}>
+      <button
+        className={`${theme !== 'dark' ? 'opacity-40' : ''}`}
+        onClick={() => (theme !== 'dark' ? setTheme('dark') : null)}
+      >
         <MoonIcon />
-      </span>
+      </button>
     </div>
     /** <Menu as="div" className="fixed top-7 md:right-16 right-5">
       <Menu.Button
