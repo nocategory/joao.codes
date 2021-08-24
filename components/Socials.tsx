ac13c91731d87c66d7c77de03ae701a28d005ef5
João <joao@joao.codes>
2021-08-24 17:15:00 +0000

    add new socials component

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,29 +1,11 @@
-import PolyworkIcon from '@components/icons/PolyworkIcon'
+import Social from '@components/Social'
 
 const Layout = (): JSX.Element => {
   return (
-    <div className="w-full flex justify-center items-center dark:invert mb-[100px] md:mb-0 transition-all duration-300 ease-in">
-      <a
-        href="http://joao.run/poly"
-        className="hover:translate-y-[-0.4rem] hover:scale-110 transition-transform ease-in px-8 py-4"
-        aria-label="Polywork"
-      >
-        <PolyworkIcon fills={false} />
-      </a>
-      <a
-        href="http://joao.run/gh"
-        className="hover:translate-y-[-0.4rem] hover:scale-110 transition-transform ease-in px-8 py-4"
-        aria-label="GitHub"
-      >
-        <img src="/icons/github.svg" alt="github" width="52" height="52" />
-      </a>
-      <a
-        href="http://joao.run/twitter"
-        className="hover:translate-y-[-0.4rem] hover:scale-110 transition-transform ease-in px-8 py-4"
-        aria-label="Twitter"
-      >
-        <img src="/icons/twitter.svg" alt="twitter" width="52" height="52" />
-      </a>
+    <div className="w-max flex justify-center items-center dark:invert bg-white/40 shadow-xl m-auto lg:mt-12 md:mt-7 sm:mt-3 rounded-2xl transition-all duration-300 ease-in">
+      <Social url="http://joao.run/poly" label="polywork" />
+      <Social url="http://joao.run/gh" label="github" />
+      <Social url="http://joao.run/twitter" label="twitter" />
     </div>
   )
 }
