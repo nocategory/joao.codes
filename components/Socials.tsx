43a97dbdbf2fce688b1367ca0d0d36fe6928efda
João <joao@joao.codes>
2021-08-24 17:17:12 +0000

    change social links

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,9 +3,9 @@ import Social from '@components/Social'
 const Layout = (): JSX.Element => {
   return (
     <div className="w-max flex justify-center items-center dark:invert bg-white/40 shadow-xl m-auto lg:mt-12 md:mt-7 sm:mt-3 rounded-2xl transition-all duration-300 ease-in">
-      <Social url="http://joao.run/poly" label="polywork" />
-      <Social url="http://joao.run/gh" label="github" />
-      <Social url="http://joao.run/twitter" label="twitter" />
+      <Social url="https://poly.joao.codes/" label="polywork" />
+      <Social url="https://github.com/nocategory" label="github" />
+      <Social url="https://twitter.com/joaodotcodes" label="twitter" />
     </div>
   )
 }
