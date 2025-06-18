fe3fd259018b4863f099ea315ed0411c06148aed
João Salgueiro <hi@joao.codes>
2025-06-18 10:43:54 +0100

    test2
    Signed-off-by: João Salgueiro <hi@joao.codes>

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default async function IndexPage() {
   return (
     <>
       <Intro />
-      <div className="md:p-7 sm:p-10 pl-5 pr-5">
+      <div className="page-container">
         <IntroText />
-        <div className="max-w-xl m-auto">
-          <h2 className="text-2xl text-[#a4a4a4] mt-10 mb-5">Some of my recent posts</h2>
+        <div className="posts-section">
+          <h2 className="posts-heading">Some of my recent posts</h2>
           {posts?.length > 0 ? (
             posts?.map(post => <PostCard key={post?.id} post={post} />)
           ) : (
